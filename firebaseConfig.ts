9764bce43b67977b0cbda3b8e8e1448492abcee8
Danilo <danilofsouza@gmail.com>
2026-04-24 15:49:07 -0300

    chore: update configuration files for environment variables and app settings

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8Il1zTFrp0LE_mIY65uErrLnVUBa_xVE',
-  authDomain: 'brasiguaios-a9ddf.firebaseapp.com',
-  projectId: 'brasiguaios-a9ddf',
-  storageBucket: 'brasiguaios-a9ddf.firebasestorage.app',
-  messagingSenderId: '138400859730',
-  appId: '1:138400859730:web:2dc98526274efb6cc0018b',
-  measurementId: 'G-3T0SKZFBBM',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
